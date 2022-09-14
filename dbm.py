368b916ac2a8d5e09e901e0351f23dfb6d39535b
keyshabellarepi <108166751+keyshabellarepi@users.noreply.github.com>
2022-09-14 15:58:51 +0700

    Add files via upload

---
+++ dbm.py
@@ -0,0 +1,24 @@
+import uuid
+import datetime
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://greatwindmill:ptm5oyCJ0FW1dFkx@gwd.boccfcn.mongodb.net/?retryWrites=true&w=majority")
+db = client.MyDatabase
+collection = db.MyCollection
+
+def save_to_db(kecepatan,latitude,longitude) -> tuple:
+    try:
+        data = {
+            "ID_Transaksi": str(uuid.uuid4()),
+            "Kecepatan": kecepatan,
+            "latitude": latitude,
+            "longitude": longitude,
+            "timestamp": datetime.datetime.now()
+        }
+        # insert data
+        rec_id1 = collection.insert_one(data)
+
+        print("Data inserted with record ids",rec_id1)
+        return True,None
+    except Exception as e:
+        return False,str(e)
